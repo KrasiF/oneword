1a7487f9cb19bfc2b56a7b77d84f5741431e7e2e
Krasimir Ferdov <62747075+KrasiF@users.noreply.github.com>
2022-10-19 12:26:25 +0200

    change app to component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,97 +1,119 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, Component, createRef, RefObject } from 'react';
 import logo from './logo.svg';
 import {io, Socket} from 'socket.io-client';
 import { CreateRoomPayload, JoinRoomPayload, RoomStatePayload } from "../../shared/roomSocketTypes"
 import { v4 as uuidv4 } from 'uuid';
-import { getSocket,setSocket} from "./service/socket";
 import ClientRoomState from '../../shared/ClientRoomState';
 
+interface AppProps{
 
+}
 
-function App() {
-  let playerId = sessionStorage.getItem("playerId");
-  if(playerId === null){
-    playerId = uuidv4();
-    sessionStorage.setItem("playerId",playerId);
+interface AppState{
+  clients: ClientRoomState[];
+  playerId?: string;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  state: AppState = {
+    clients: []
   }
-  let socketQuery = getSocket();
-  let socket: Socket = socketQuery !== null? socketQuery: setSocket(playerId);
+  nicknameInputRef: RefObject<HTMLInputElement>;
+  roomIdInputRef: RefObject<HTMLInputElement>;
+  socket: Socket;
 
-  const roomIdInputRef = useRef<any>("");
-  const nicknameInputRef = useRef<any>("");
-  const [clients,setClients] = useState<Array<ClientRoomState>>([]);
-  socket.on("joined_room",(data)=>{
-    onRoomJoined(JSON.parse(data));
-  });
+  constructor(props: AppProps){
+    super(props);
+    this.socket = this.setupSocket();
+    this.nicknameInputRef = React.createRef<HTMLInputElement>();
+    this.roomIdInputRef = React.createRef<HTMLInputElement>();
+  }
 
-  socket.on("state_update_room",(data)=>{
-    onRoomUpdated(JSON.parse(data));
-  });
 
-  socket.on("left_room", ()=>{
-    setClients([]);
-  });
+  setupSocket(): Socket {
+    let playerId = sessionStorage.getItem("playerId");
+    if(playerId === null){
+      playerId = uuidv4();
+      sessionStorage.setItem("playerId",playerId);
+    }
+    let socket: Socket = io("http://localhost:3001", { query: { "playerId": playerId } });
+    this.setupSocketConnections(socket);
+    return socket;
+  }
 
-  // socket.on("receive_playerId",(id)=>{
-  //   console.log("received playerId");
-  //   if(sessionStorage.getItem("playerId")==null){
-  //     sessionStorage.setItem("playerId",id);
-  //     console.log("playerId set");
-  //   }
-  // });
+  setupSocketConnections(socket:Socket){
+    socket.on("joined_room",(data)=>{
+      console.log("joined "+data);
+      this.onRoomJoined(JSON.parse(data));
+    });
+  
+    socket.on("state_update_room",(data)=>{
+      console.log("stateupdate "+data);
+      this.onRoomUpdated(JSON.parse(data));
+    });
+  
+    socket.on("left_room", ()=>{
+      this.setState({clients:[]});
+    });
+  }
 
-  return (
-    <div className="App">
-      <label>Nickname:</label>
-      <input type="text" ref={nicknameInputRef}></input>
-      <button onClick={()=>{createRoom()}}>Create Room</button>
-      <input type="text" ref={roomIdInputRef}></input>
-      <button onClick={()=>{joinRoom()}}>Join Room</button>
-      <button onClick={()=>{leaveRoom()}}>Leave Room</button>
-      {
-        clients.map((c)=><p>{c.nickname}</p>)
-      }
-    </div>
-  );
-
-  function getNickname() : string {
-    let inputVal = nicknameInputRef.current.value;
+  getNickname() : string | null {
+    let inputVal = this.nicknameInputRef.current !== null? this.nicknameInputRef.current.value : null;
     return inputVal;
   }
 
-  function onRoomUpdated(payload: RoomStatePayload){
+  onRoomUpdated(payload: RoomStatePayload){
     payload.clients.forEach((c)=>console.log(c));
-    setClients(payload.clients);
+    this.setState({clients:payload.clients});
   }
 
-  function onRoomJoined(payload: RoomStatePayload){
-    payload.clients.forEach((c)=>console.log(c));
-    setClients(payload.clients);
-    roomIdInputRef.current.value = payload.roomId;
+  onRoomJoined(payload: RoomStatePayload){
+    if(this.roomIdInputRef.current){
+      this.roomIdInputRef.current.value = payload.roomId;
+    }
+
+    this.setState({clients:payload.clients});
+    console.log("payload id: "+ payload.roomId)
   }
 
-  function createRoom() {
-    let nickname = getNickname();
+  createRoom() {
+    let nickname = this.getNickname();
     if(nickname == null || nickname.length == 0){
       return;
     }
     const payload: CreateRoomPayload = {nickname: nickname}
-    socket.emit("create_room",JSON.stringify(payload));
+    this.socket.emit("create_room",JSON.stringify(payload));
   }
 
-  function joinRoom(){
-    let nickname = getNickname();
-    let inputVal = roomIdInputRef.current.value;
+  joinRoom(){
+    let nickname = this.getNickname();
+    let inputVal = this.roomIdInputRef.current != null ? this.roomIdInputRef.current.value : null;
 
     if(nickname == null || nickname.length == 0 || inputVal == null){
       return;
     }
     const joinRoomPayload: JoinRoomPayload = {roomId: inputVal, nickname: nickname};
-    socket.emit("join_room",JSON.stringify(joinRoomPayload));
+    this.socket.emit("join_room",JSON.stringify(joinRoomPayload));
   }
 
-  function leaveRoom(){
-    socket.emit("leave_room");
+  leaveRoom(){
+    this.socket.emit("leave_room");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <label>Nickname:</label>
+        <input type="text" ref={this.nicknameInputRef}></input>
+        <button onClick={()=>{this.createRoom()}}>Create Room</button>
+        <input type="text" ref={this.roomIdInputRef}></input>
+        <button onClick={()=>{this.joinRoom()}}>Join Room</button>
+        <button onClick={()=>{this.leaveRoom()}}>Leave Room</button>
+        {
+          this.state.clients.map((c)=><p>{c.nickname}</p>)
+        }
+      </div>
+    );
   }
 }
 
